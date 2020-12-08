729d5917005f470e80d5dd99a7502fbb267ea2a7
Ryna Truong <rynatruong@outlook.com>
2020-12-08 21:35:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
+  configure do 
+    enable :sessions
+    set :sessions_secret, "secret"
+  end
+  
   get '/' do
     erb :index
   end
